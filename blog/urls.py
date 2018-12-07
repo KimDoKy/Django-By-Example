41d96190668797026821b1b308f95d20bd3eec1a
Kim Do Ky <makingfunk0@gmail.com>
2018-12-07 13:05:30 +0900

    sending email with django

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<year>/<month>/<day>/<post>/', views.post_detail, name='post_detail'),
+    path('<post_id>/share/', views.post_share, name='post_share'),
     ]
