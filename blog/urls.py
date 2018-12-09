c83909a5cca812f0a2698231f00b38f933c35a33
Kim Do Ky <makingfunk0@gmail.com>
2018-12-09 15:21:36 +0900

    adding tagging functionality

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,9 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='post_list'),
+#    path('', views.PostListView.as_view(), name='post_list'),
+    path('', views.post_list, name='post_list'),
+    path('tag/<tag_slug>/', views.post_list, name='post_list_by_tag'),
     path('<year>/<month>/<day>/<post>/', views.post_detail, name='post_detail'),
     path('<post_id>/share/', views.post_share, name='post_share'),
     ]
