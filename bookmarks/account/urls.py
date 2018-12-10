08234fdb622f758bd51cfa75efeba7a057918f7c
Kim Do Ky <makingfunk0@gmail.com>
2018-12-10 20:45:53 +0900

    Change password views

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # previous login view
-    # path('login/', views.user_login, name='login'),
     path('', views.dashboard, name='dashboard'),
     ]
